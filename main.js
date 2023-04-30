ff149cc733020def41ab15a2f32698826cd87ebf
Underwater008 <69733615+Underwater008@users.noreply.github.com>
2023-04-30 04:03:33 -0400

    update

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import * as THREE from './node_modules/three/build/three.module.js';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { GLTFLoader } from './node_modules/three/examples/jsm/loaders/GLTFLoader';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
