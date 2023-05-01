3b46a5616b75af9cbd587276b33f5caa377f6cae
Underwater008 <69733615+Underwater008@users.noreply.github.com>
2023-05-01 04:55:09 -0400

    Much more

--- main.js
+++ main.js
@@ -2,12 +2,14 @@ import * as THREE from 'three';
 import { OrbitControls } from './node_modules/three/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from './node_modules/three/examples/jsm/loaders/GLTFLoader';
 
-const showLoadingScreen = true;
+const showLoadingScreen = false;
 const firstVisit = localStorage.getItem("firstVisit") === null;
 
 const scene = new THREE.Scene();
+
 const myCamera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
+
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setClearColor(0x000000, 0);
 document.body.appendChild(renderer.domElement);
@@ -26,7 +28,8 @@ let model;
 loader.load('./3DModels/cozy_campfire_-_shape_key_animation/scene.gltf', (gltf) => {
   model = gltf.scene;
   scene.add(model);
-  const fireLight = new THREE.PointLight(0xE31937, 2, 50);
+  model.position.x = -5;
+  const fireLight = new THREE.PointLight(0xCD3B40, 2, 50);
   model.add(fireLight);
   model.traverse((node) => {
     if (node.isMesh) {
@@ -48,7 +51,11 @@ loader.load('./3DModels/cozy_campfire_-_shape_key_animation/scene.gltf', (gltf) 
 // pointLight.position.set(10, 10, 10);
 // scene.add(pointLight);
 
-myCamera.position.z = 5;
+myCamera.position.x = 30;
+myCamera.position.y = 60;
+myCamera.position.z = 80;
+
+const myCameraPos = myCamera.position;
 
 function animate() {
   requestAnimationFrame(animate);
@@ -58,6 +65,9 @@ function animate() {
 
   // Render the scene
   renderer.render(scene, myCamera);
+
+  console.log(myCamera.position);
+
 }
 
 // Call the animate function
@@ -90,31 +100,49 @@ const stellaruneBtn = document.getElementById("stellarune-btn");
 const freefall2Btn = document.getElementById("freefall2-btn");
 const blogsBtn = document.getElementById("blogs-btn");
 const aboutBtn = document.getElementById("About-btn");
+const HomoLudenBtn = document.getElementById("homo-ludens");
 
 resetBtn.addEventListener("click", () => {
+  resetLandingPage();
   rotateModel(0, 0, 0); // Reset the model's rotation
-  TweenMax.to(myCamera.position, 1, { x: 0, y: 0, z: 5 });
+  TweenMax.to(myCamera.position, 1, { x: 30, y: 60, z: 80 });
 });
 
 stellaruneBtn.addEventListener("click", () => {
+  resetLandingPage();
   rotateModel(0, Math.PI / 4, 0); // Rotate the model to a certain degree
   TweenMax.to(myCamera.position, 1, { x: 0, y: 0, z: 5 });
 });
 
 freefall2Btn.addEventListener("click", () => {
+  resetLandingPage();
   rotateModel(0, Math.PI / 2, 0); // Rotate the model to another degree
     TweenMax.to(myCamera.position, 1, { x: 0, y: 0, z: 5 });
 });
 
 blogsBtn.addEventListener("click", () => {
+  resetLandingPage();
   rotateModel(0, (3 * Math.PI) / 4, 0); // Rotate the model to another degree
   TweenMax.to(myCamera.position, 1, { x: 0, y: 0, z: 5 });
 });
 
 aboutBtn.addEventListener("click", () => {
+  resetLandingPage();
   rotateModel(0, (4 * Math.PI) / 4, 0); // Rotate the model to another degree
   TweenMax.to(myCamera.position, 1, { x: 0, y: 0, z: 5 });
 });
+
+const infoDiv = document.getElementById('homo-ludens-info');
+HomoLudenBtn.addEventListener("click", () => {
+  resetLandingPage();
+  rotateModel(0, (4 * Math.PI) / 4, 0); // Rotate the model to another degree
+  TweenMax.to(myCamera.position, 1, { x: -10.9, y: 3.7, z: 3.4 });
+  infoDiv.style.display = 'block';
+});
+
+function resetLandingPage(){
+  infoDiv.style.display = 'none';
+}
 
 // Loading screen logic
 // Loading screen logic
